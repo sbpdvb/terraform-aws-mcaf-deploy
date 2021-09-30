fd7aff1d5b669d3f452f65cda73ccef2ebee7932
Dennis Brouwer <dbrouwer@schubergphilis.com>
2021-09-30 10:02:24 +0200

    Fix usecase repository

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   usecase            = "${var.usecase_environment}-${var.usecase_name}"
   usecase_bucket     = "${local.usecase}-deploy"
-  usecase_repository = "dvb-${var.usecase_code}"
+  usecase_repository = "${var.usecase-environment}-${var.usecase_name}"
   usecase_version    = trimspace(var.usecase_version)
 }
 

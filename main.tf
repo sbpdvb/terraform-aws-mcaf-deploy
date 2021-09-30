cfa0ad402ada7194faa69ae0017d37cb3b119cc8
Dennis Brouwer <dbrouwer@schubergphilis.com>
2021-09-30 10:24:18 +0200

    Test without trigger

--- main.tf
+++ main.tf
@@ -8,29 +8,29 @@ locals {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-data "github_release" "default" {
-  repository  = local.usecase_repository
-  owner       = var.usecase_repository_owner
-  retrieve_by = "tag"
-  release_tag = local.usecase_version
-}
+# data "github_release" "default" {
+#   repository  = local.usecase_repository
+#   owner       = var.usecase_repository_owner
+#   retrieve_by = "tag"
+#   release_tag = local.usecase_version
+# }
 
 data "aws_subnet" "private" {
   id = var.subnet_ids[0]
 }
 
-resource "null_resource" "trigger" {
-  triggers = {
-    release_id = data.github_release.default.id
-  }
+# resource "null_resource" "trigger" {
+#   triggers = {
+#     release_id = data.github_release.default.id
+#   }
 
-  provisioner "local-exec" {
-    command = "${path.module}/bin/trigger -project-name=${local.usecase} -source-version=${local.usecase_version}"
-  }
+#   provisioner "local-exec" {
+#     command = "${path.module}/bin/trigger -project-name=${local.usecase} -source-version=${local.usecase_version}"
+#   }
 
-  depends_on = [
-    aws_codebuild_project.deploy_functions,
-    aws_codebuild_project.source,
-    aws_codepipeline.codepipeline,
-  ]
-}
+#   depends_on = [
+#     aws_codebuild_project.deploy_functions,
+#     aws_codebuild_project.source,
+#     aws_codepipeline.codepipeline,
+#   ]
+# }

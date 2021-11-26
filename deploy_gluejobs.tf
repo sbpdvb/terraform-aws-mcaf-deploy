868fc42f0d097d4073794758784cc324998bf4c1
Dennis Brouwer <dbrouwer@schubergphilis.com>
2021-11-26 12:52:31 +0100

    Switch to dynamic environment variable for Glue ARN

--- deploy_gluejobs.tf
+++ deploy_gluejobs.tf
@@ -120,11 +120,14 @@ resource "aws_codebuild_project" "deploy_gluejobs" {
       name  = "WORKLOAD_VERSION"
       value = var.workload_version
     }
-    environment_variable {
-      name  = "GLUE_IAM_ROLE"
-      value = var.gluejob_iam_role_arn
-    }
 
+    dynamic "environment_variable" {
+      for_each = var.gluejob_iam_role_arn == "" ? [] : [1]
+      content {
+        name  = "GLUE_IAM_ROLE"
+        value = var.gluejob_iam_role_arn
+      }
+    }
     dynamic "environment_variable" {
       for_each = var.environment_variables
       content {

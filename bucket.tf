69a8c152f111dfbfcd5fe64314fbbd4a22f5bd6a
Enri Peters <epeters@schubergphilis.com>
2023-06-07 14:23:12 +0200

    tfsec improvements

--- bucket.tf
+++ bucket.tf
@@ -1,8 +1,9 @@
+# tfsec:ignore:aws-s3-enable-bucket-logging
 module "deployment_bucket" {
   source        = "github.com/schubergphilis/terraform-aws-mcaf-s3?ref=v0.5.0"
   name          = local.deployment_bucket
   force_destroy = true
   versioning    = true
   tags          = var.tags
-  logging       = var.logging # tfsec:ignore:aws-s3-enable-bucket-logging
+  logging       = var.logging
 }
